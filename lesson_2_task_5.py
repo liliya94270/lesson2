eac1753e1a4b07fdd96522644bbd25c9425b106b
liliya94270 <liya.khabibullina.10@mail.ru>
2025-02-17 14:30:37 +0500

    Update lesson_2_task_5.py

--- lesson_2_task_5.py
+++ lesson_2_task_5.py
@@ -1,15 +1,13 @@
 def month_to_season(month):
-    if month in [12, 1, 2]:
-        return "зима"
-    elif month in [3, 4, 5]:
-        return "весна"
-    elif month in [6, 7, 8]:
-        return "лето"
-    elif month in [9, 10, 11]:
-        return "осень"
-    else:
-        return "некорректный номер месяца"
+    seasons = {
+        (12, 1, 2): "зима",
+        (3, 4, 5): "весна",
+        (6, 7, 8): "лето",
+        (9, 10, 11): "осень",
+    }
+    for key, value in seasons.items():
+        if month in key:
+            return value
+    return "некорректный номер месяца"
 
-month = int(input("Введите номер месяца (1-12): "))
-season = month_to_season(month)
-print(f"Месяц {month} относится к сезону: {season}")
+print(f"Месяц {month} относится к сезону: {month_to_season(int(input('Введите номер месяца (1-12): ')))}")
